1a154b9815fff7906f83941c89ab01e4ff92375f
Ben Christel <ben.christel@gmail.com>
2024-06-02 19:00:32 -0700

    Begin HTML generation

--- html.test.js
+++ html.test.js
@@ -1,7 +1,45 @@
-import {test, expect, is} from "@benchristel/taste"
+import {test, expect} from "@benchristel/taste"
+import {generateHtml} from "./html.js"
 
-test("the tests", {
-  "work"() {
-    expect(1, is, 1)
+test("the webring html", {
+  "points all links to the hub when there are no sites in the webring"() {
+    const config = {
+      "name": "Test ring",
+      "configLocation": "https://example.com/ring.json",
+      "hub": "https://example.com",
+      "members": []
+    }
+
+    const expected = `
+      <div class="wwwebring-widget">
+        <div class="wwwebring-start">
+          <a href="https://example.com">Test ring</a>
+        </div>
+        <div class="wwwebring-middle">
+          <a href="https://example.com">Test ring</a>
+        </div>
+        <div class="wwwebring-end">
+          <a href="https://example.com">Test ring</a>
+        </div>
+      </div>
+    `
+
+    const html = generateHtml(config, "https://irrelevant.com")
+
+    expect(html, isDisregardingSpaces, expected)
   }
 })
+
+function isDisregardingSpaces(rawExpected, rawActual) {
+  if (typeof rawExpected !== "string") return false;
+  if (typeof rawActual !== "string") return false;
+
+  const expected = stripSpacesFromHTML(rawExpected)
+  const actual = stripSpacesFromHTML(rawActual)
+
+  return expected === actual
+}
+
+function stripSpacesFromHTML(html) {
+  return html.replace(/\s+/g, " ").replace(/ ?([<>]) ?/g, "$1")
+}
