b4df97ebc0e803a18f195742c0d39f03942a071e
Ben Christel <ben.christel@gmail.com>
2024-06-03 15:27:50 -0700

    Make Ring a class

--- src/domain/ring.js
+++ src/domain/ring.js
@@ -1,11 +1,35 @@
 
 export function model(config) {
-  return {
-    prevUrl: config.hub,
-    prevTitle: config.name,
-    hubUrl: config.hub,
-    hubTitle: config.name,
-    nextUrl: config.hub,
-    nextTitle: config.name,
+  return new Ring(config)
+}
+
+export class Ring {
+  constructor(config, currentUrl) {
+    this.config = config;
+    this.currentUrl = currentUrl;
+  }
+
+  get prevUrl() {
+    return this.config.hub;
+  }
+
+  get prevTitle() {
+    return this.config.name;
+  }
+
+  get hubUrl() {
+    return this.config.hub;
+  }
+
+  get hubTitle() {
+    return this.config.name;
+  }
+
+  get nextUrl() {
+    return this.config.hub;
+  }
+
+  get nextTitle() {
+    return this.config.name;
   }
 }
--- src/domain/ring.test.js
+++ src/domain/ring.test.js
@@ -1,24 +1,32 @@
 import {test, expect, equals} from "@benchristel/taste"
-import {model} from "./ring.js"
+import {Ring} from "./ring.js"
 
-test("model", {
-  "points all links to the hub when the webring has no members"() {
-    const config = {
-      "name": "Test ring",
-      "configLocation": "https://example.com/ring.json",
-      "hub": "https://example.com",
-      "members": []
-    }
+const baseConfig = {
+  "name": "Test ring",
+  "configLocation": "https://example.com/ring.json",
+  "hub": "https://example.com",
+  "members": []
+}
 
-    const theModel = model(config, "https://irrelevant.com")
+test("a webring with no members", {
+  "points the 'prev' link to the hub"() {
+    const ring = new Ring(baseConfig, "https://irrelevant.com")
 
-    expect(theModel, equals, {
-      prevUrl: "https://example.com",
-      prevTitle: "Test ring",
-      hubUrl: "https://example.com",
-      hubTitle: "Test ring",
-      nextUrl: "https://example.com",
-      nextTitle: "Test ring"
-    })
-  }
+    expect(ring.prevUrl, equals, "https://example.com")
+    expect(ring.prevTitle, equals, "Test ring")
+  },
+
+  "points the 'next' link to the hub"() {
+    const ring = new Ring(baseConfig, "https://irrelevant.com")
+
+    expect(ring.nextUrl, equals, "https://example.com")
+    expect(ring.nextTitle, equals, "Test ring")
+  },
+
+  "points the 'hub' link to the hub"() {
+    const ring = new Ring(baseConfig, "https://irrelevant.com")
+
+    expect(ring.hubUrl, equals, "https://example.com")
+    expect(ring.hubTitle, equals, "Test ring")
+  },
 })
